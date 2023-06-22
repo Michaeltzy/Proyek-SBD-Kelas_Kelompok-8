89735557b8b14312777c16ecc5d05d7070b6790c
Eriqo90AW <eriqo.arief@ui.ac.id>
2023-06-22 22:58:55 +0700

    add images

---
+++ Kode Java WordCount/wordcount_1GB.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+public class wordcount_1GB {
+    public static void main(String args[]) throws Exception {
+
+        System.out.println("Counting Words");
+        FileReader fr = new FileReader("gutenberg-1G.txt"); //membaca file 
+        BufferedReader br = new BufferedReader(fr); //deklarasi br sebagai buffer reader
+        String line = br.readLine();
+        Map<String, Integer> wordCountMap = new HashMap<>();
+
+        while (line != null) {
+            String[] words = line.split("\\s+");
+            for (String word : words) {
+                wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+            }
+            line = br.readLine();
+        }
+
+        for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
+            System.out.println(entry.getKey() + ", Count: " + entry.getValue());
+        }
+        br.close();
+        fr.close();
+    }
+}
